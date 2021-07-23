5c834f067808294b817725cdec7fe8caa0affd74
Kirill-Petrovskiy <petrovskiykirillur@gmail.com>
2021-07-23 13:34:23 +0300

    Lessin 1 v002

--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -6,14 +6,14 @@ puts "Введите коэффициент c"
 c = gets.chomp.to_i
 
 d = b**2 - 4 * a * c
+c = Math.sqrt(d)
 if d > 0
-  c = Math.sqrt(d)
   x1 = (-b + c)/(2 * a)
   x2 = (-b - c)/(2 * a)
   puts "x1 = #{x1} x2 = #{x2}"
-    elsif d < 0
-    puts "Корней уравнения нет"
-      else
-      x1 = x2 = -b/(2 * a)
-      puts "x1 = #{x1} x2= #{x2}"
+elsif d < 0
+  puts "Корней уравнения нет"
+else
+  x1 = x2 = -b/(2 * a)
+  puts "x1 = #{x1} x2= #{x2}"
 end
